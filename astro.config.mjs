288855eb06bb9c391211c097c684038959d8d2b1
Juan Vladimir <juanvladimir13@gmail.com>
2025-06-08 12:07:20 -0400

    refactor view transition

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,16 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 
 export default defineConfig({
   vite: {
     plugins: [yaml()]
+  },
+  env: {
+    schema: {
+      API_URL: envField.string({ context: "client", access: "public", default: "example" }),
+      PORT: envField.number({ context: "client", access: "public", optional: true }),
+      API_SECRET: envField.string({ context: "server", access: "secret" }),
+    }
   }
 });
